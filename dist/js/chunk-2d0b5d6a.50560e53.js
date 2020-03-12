837af6ca2d7360c3c30d8cacd5a28143dd7f4053
zhaoyi <yzhao@modreat.com>
2020-03-12 21:31:35 +0800

    dist

---
+++ dist/js/chunk-2d0b5d6a.50560e53.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b5d6a"],{"1b36":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h2",[e._v("敬请期待....")])])}],u={},l=u,a=t("2877"),s=Object(a["a"])(l,c,r,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0b5d6a.50560e53.js.map
